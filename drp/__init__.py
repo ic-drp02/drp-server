c112d0a1f4ccbcde9c6b9aab45d7c1c6919a23d9
Hasan Ali <git@hasali.co.uk>
2020-05-27 19:09:48 +0100

    Move initialisation into create_app factory

---
+++ drp/__init__.py
@@ -0,0 +1,43 @@
+from flask import Flask, escape, request
+
+from . import config
+from .db import db
+
+
+def create_app(test_config=None):
+    app = Flask(__name__)
+
+    if test_config is None:
+        app.config["SQLALCHEMY_DATABASE_URI"] = config.DATABASE_URI
+        app.config["SQLALCHEMY_ECHO"] = config.IS_DEV_ENV
+        app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+    else:
+        app.config.from_mapping(test_config)
+
+    db.init_app(app)
+
+    @app.route("/")
+    def hello():
+        name = request.args.get("name", "World")
+        return f"Hello, {escape(name)}!"
+
+    # Example database code
+    #
+    # from flask import jsonify
+    #
+    # class User(db.Model):
+    #     id = db.Column(db.Integer, primary_key=True)
+    #     username = db.Column(db.String(80), unique=True, nullable=False)
+    #     email = db.Column(db.String(120), unique=True, nullable=False)
+
+    #     def __repr__(self):
+    #         return f"<User {self.username}>"
+    #
+    #
+    # @app.route("/users/")
+    # def get_users():
+    #     users = [{'username': user.username, 'email': user.email}
+    #              for user in User.query.all()]
+    #     return jsonify(users)
+
+    return app
--- wsgi.py
+++ wsgi.py
@@ -1,4 +1,6 @@
-from drp.app import app
+from drp import create_app
+
+app = create_app()
 
 if __name__ == "__main__":
     app.run()
