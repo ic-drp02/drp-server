f5b31fd269ed03004f270011685fa80062b6c1ae
Hasan Ali <git@hasali.co.uk>
2020-05-31 17:38:54 +0100

    Add users table

--- drp/models/__init__.py
+++ drp/models/__init__.py
@@ -1,6 +1,8 @@
 from .question import Question, Site, Subject, Grade
 from .post import Post, Tag, File
 from .device import Device
+from .user import User, UserRole
 
 __all__ = ["Post", "Tag", "File", "Question",
-           "Site", "Subject", "Grade", "Device"]
+           "Site", "Subject", "Grade", "Device",
+           "User", "UserRole"]
