6fb0b9c7bc314088b90246949680f4c76084720b
Hasan Ali <git@hasali.co.uk>
2020-06-01 10:05:16 +0100

    Add tests for questions api

---
+++ tests/test_questions.py
@@ -0,0 +1,98 @@
+import json
+
+from drp.models import Question, Site, Subject
+
+
+def test_get_all_questions(app, db):
+    site = Site(name="Site 1")
+    subject = Subject(name="Subject 1")
+
+    with app.app_context():
+        db.session.add(site)
+        db.session.add(subject)
+
+        db.session.add(Question(site=site,
+                                specialty="Specialty 1",
+                                subject=subject,
+                                text="An example question..."))
+
+        db.session.add(Question(site=site,
+                                specialty="Specialty 2",
+                                subject=subject,
+                                text="Another question..."))
+
+        db.session.commit()
+
+    with app.test_client() as client:
+        response = client.get("/api/questions")
+
+        assert "200" in response.status
+
+        data = json.loads(response.data.decode("utf-8"))
+
+        assert len(data) == 2
+
+        assert data[0]["site"]["name"] == "Site 1"
+        assert data[0]["specialty"] == "Specialty 1"
+        assert data[0]["subject"]["name"] == "Subject 1"
+        assert data[0]["text"] == "An example question..."
+
+        assert data[1]["site"]["name"] == "Site 1"
+        assert data[1]["specialty"] == "Specialty 2"
+        assert data[1]["subject"]["name"] == "Subject 1"
+        assert data[1]["text"] == "Another question..."
+
+
+def test_create_question(app, db):
+    site = Site(name="Site 1")
+    subject = Subject(name="Subject 1")
+
+    with app.app_context():
+        db.session.add(site)
+        db.session.add(subject)
+        db.session.commit()
+
+    question = {
+        "site": "Site 1",
+        "specialty": "Specialty 1",
+        "subject": "Subject 1",
+        "text": "An example question"
+    }
+
+    with app.test_client() as client:
+        response = client.post("/api/questions", json=question)
+
+        assert "200" in response.status
+
+        data = json.loads(response.data.decode("utf-8"))
+
+        assert data["site"]["name"] == question["site"]
+        assert data["specialty"] == question["specialty"]
+        assert data["subject"]["name"] == question["subject"]
+        assert data["text"] == question["text"]
+
+
+def test_delete_question(app, db):
+    with app.app_context():
+        site = Site(name="Site 1")
+        subject = Subject(name="Subject 1")
+
+        question = Question(site=site,
+                            specialty="Specialty 1",
+                            subject=subject,
+                            text="An example question...")
+
+        db.session.add(site)
+        db.session.add(subject)
+        db.session.add(question)
+
+        db.session.commit()
+
+        id = question.id
+
+    with app.test_client() as client:
+        response = client.delete(f"/api/questions/{id}")
+        assert "204" in response.status
+
+    with app.app_context():
+        assert Question.query.count() == 0
