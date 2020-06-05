0df3aefc62ff184421aa4e948e004c126b81f7be
Hasan Ali <git@hasali.co.uk>
2020-06-05 20:56:46 +0100

    Add devices table

--- drp/models/__init__.py
+++ drp/models/__init__.py
@@ -1,4 +1,6 @@
 from .question import Question, Site, Subject, Grade
 from .post import Post, Tag, File
+from .device import Device
 
-__all__ = ["Post", "Tag", "File", "Question", "Site", "Subject", "Grade"]
+__all__ = ["Post", "Tag", "File", "Question",
+           "Site", "Subject", "Grade", "Device"]
