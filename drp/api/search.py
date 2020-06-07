c272b0e4ad55676a5f037ab08680f044483ac4f8
Adam Dejl <adam.dejl18@imperial.ac.uk>
2020-06-07 22:45:38 +0200

    Implement search results pagination

--- drp/api/search.py
+++ drp/api/search.py
@@ -1,13 +1,19 @@
 from sqlalchemy import text
 from sqlalchemy.sql import func
 
-from flask_restful import Resource
+from flask import request
+from flask_restful import Resource, abort
 
 from .posts import serialize_post
 
 from ..models import Post
 
 from ..db import db
+
+
+def extract_results(query):
+    return [serialize_post(result[0])
+            for result in query.all()]
 
 
 class PostSearchResource(Resource):
@@ -21,6 +27,14 @@ class PostSearchResource(Resource):
             in: path
             type: string
             required: true
+          - name: page
+            in: query
+            type: number
+            required: false
+          - name: results_per_page
+            in: query
+            type: number
+            required: false
         responses:
           200:
             schema:
@@ -28,14 +42,28 @@ class PostSearchResource(Resource):
               items:
                 $ref: "#/definitions/Post"
         """
+        page = request.args.get("page")
+        results_per_page = request.args.get("results_per_page")
+
         # Construct text search query
         ts_query = func.plainto_tsquery('english', searched)
         # Compute rank for each search result
         ts_rank = func.ts_rank_cd(Post.__ts_vector__, ts_query).label("rank")
         # Query for the search results ordered by rank
-        results = db.session.query(Post, ts_rank) \
+        query = db.session.query(Post, ts_rank) \
             .filter(Post.__ts_vector__.op('@@')(ts_query)) \
-            .order_by(text("rank desc")) \
-            .all()
-        return [serialize_post(result[0])
-                for result in results]
+            .order_by(text("rank desc"))
+
+        if page is None or results_per_page is None:
+            return extract_results(query)
+
+        if not page.isdigit() or not results_per_page.isdigit():
+            return abort(400, message="Page and results_per_page fields must "
+                         "be numbers.")
+
+        page = int(page)
+        results_per_page = int(results_per_page)
+
+        query = query.limit(results_per_page).offset(page * results_per_page)
+
+        return extract_results(query)
