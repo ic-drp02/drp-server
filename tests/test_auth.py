9a66343c6ac91335924d32d4bdba5f690820fa9a
Hasan Ali <git@hasali.co.uk>
2020-06-09 20:40:05 +0100

    Fix broken authentication tests

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -4,29 +4,29 @@ from argon2 import PasswordHasher
 from drp.models import User
 
 
-def create_user(app, db, username, password):
+def create_user(app, db, email, password):
     with app.app_context():
         hasher = PasswordHasher()
         hash = hasher.hash(password)
 
-        user = User(username=username, password_hash=hash)
+        user = User(email=email, password_hash=hash, confirmed=True)
 
         db.session.add(user)
         db.session.commit()
 
 
-def test_authentication_for_valid_username_and_password(app, db):
-    username = "username"
+def test_authentication_for_valid_email_and_password(app, db):
+    email = "test@nhs.net"
     password = "some secure password, but who am I kidding :("
-    create_user(app, db, username, password)
+    create_user(app, db, email, password)
 
     with app.test_client() as client:
         credentials = {
-            "username": username,
+            "email": email,
             "password": password
         }
 
-        response = client.post("/api/authenticate", json=credentials)
+        response = client.post("/auth/authenticate", json=credentials)
 
         assert "200" in response.status
 
@@ -35,27 +35,27 @@ def test_authentication_for_valid_username_and_password(app, db):
         assert "token" in data
 
 
-def test_authentication_for_invalid_username_and_password(app, db):
-    username = "username"
+def test_authentication_for_invalid_email_and_password(app, db):
+    email = "test@nhs.net"
     password = "some secure password, but who am I kidding :("
-    create_user(app, db, username, password)
+    create_user(app, db, email, password)
 
     with app.test_client() as client:
         credentials = {
-            "username": "rando69",
+            "email": "rando69@nhs.net",
             "password": password
         }
 
-        response = client.post("/api/authenticate", json=credentials)
+        response = client.post("/auth/authenticate", json=credentials)
 
         assert "401" in response.status
 
     with app.test_client() as client:
         credentials = {
-            "username": username,
+            "email": email,
             "password": "password123"
         }
 
-        response = client.post("/api/authenticate", json=credentials)
+        response = client.post("/auth/authenticate", json=credentials)
 
         assert "401" in response.status
