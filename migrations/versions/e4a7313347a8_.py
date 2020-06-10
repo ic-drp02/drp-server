75dfe5a57b248f56174adec16a032220a254ea11
Hasan Ali <git@hasali.co.uk>
2020-06-10 18:29:58 +0100

    Merge migration heads

---
+++ migrations/versions/e4a7313347a8_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: e4a7313347a8
+Revises: 55da4590bacd, e942b0db946d
+Create Date: 2020-06-10 17:34:43.805093
+
+"""
+# from alembic import op
+# import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'e4a7313347a8'
+down_revision = ('55da4590bacd', 'e942b0db946d')
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
