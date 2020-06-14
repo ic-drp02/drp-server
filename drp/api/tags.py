6b1aba0293e33e3292789d786b9da06c752263fd
Hasan Ali <git@hasali.co.uk>
2020-06-14 16:57:11 +0100

    Add support for tag renaming

--- drp/api/tags.py
+++ drp/api/tags.py
@@ -27,6 +27,55 @@ def serialize_tag(tag):
 
 class TagResource(Resource):
 
+    def put(self, id):
+        """
+        Updates a single tag by id.
+        ---
+        parameters:
+          - name: id
+            in: path
+            type: integer
+            required: true
+          - in: body
+            name: post
+            schema:
+              type: object
+              properties:
+                name:
+                  type: string
+                  required: true
+                  maxLength: 30
+        responses:
+          204:
+            description: Success
+          404:
+            description: Not found
+        """
+        body = request.json
+
+        name = body.get("name")
+
+        tag = Tag.query.filter(Tag.id == id).one_or_none()
+        if tag is None:
+            return abort(404)
+
+        if name is not None:
+            if len(name) > 30:
+                return abort(400, message="`name` must not be more than 30"
+                             " characters.")
+            tag.name = name
+
+        try:
+            db.session.commit()
+        except IntegrityError as err:
+            if err.orig.pgcode == "23505":
+                return abort(422,
+                             message="A tag with this name already exists.")
+            else:
+                raise
+
+        return serialize_tag(tag)
+
     def delete(self, id):
         """
         Deletes a single tag by id.
