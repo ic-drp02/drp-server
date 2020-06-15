6649dc5aceb9cfc685a3ae9443e8e7ea356bbb16
Adam Dejl <adam.dejl18@imperial.ac.uk>
2020-06-15 18:40:59 +0200

    Fix database migrations

---
+++ migrations/versions/b936807b20cf_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: b936807b20cf
+Revises: fc2597055c79, 88c687971a14
+Create Date: 2020-06-15 18:39:15.638466
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'b936807b20cf'
+down_revision = ('fc2597055c79', '88c687971a14')
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
